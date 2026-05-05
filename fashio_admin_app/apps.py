03c4a002f345d71b195a1389a4250fc0c5110c27
Fayyad <fayyadhydervt123@gmail.com>
2026-05-05 10:16:03 +0530

    refactor: remove old app folders after renaming

--- fashio_admin_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FashioAdminAppConfig(AppConfig):
-    name = 'fashio_admin_app'
--- fashio_user_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FashioUserAppConfig(AppConfig):
-    name = 'fashio_user_app'
